17fa14e5c8d4dcf3c023d7d3e7ed1a30305e8aa4
Jazastry <yogib@mail.bg>
2015-04-15 14:42:45 +0300

    new try

--- Chat.Client/Scripts/ajax-requester.js
+++ Chat.Client/Scripts/ajax-requester.js
@@ -4,6 +4,7 @@ var ajaxRequester = (function () {
 
     // baseUrl must be changed when deploying application
     //var baseUrl = "http://localhost:44965/";
+
     var baseUrl = "http://lavander-chat.apphb.com/";
 
     var headers = {};
