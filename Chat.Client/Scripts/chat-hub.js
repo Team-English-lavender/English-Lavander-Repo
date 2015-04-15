e1295e6611873652860c02baaa298a40ccaaa430
Mina <mina.dodunekova@gmail.com>
2015-04-15 13:26:23 +0300

    add jquery reference in the Self Invoking function

--- Chat.Client/Scripts/chat-hub.js
+++ Chat.Client/Scripts/chat-hub.js
@@ -1,28 +1,31 @@
-﻿$(function () {
-    var connection = $.hubConnection();
-    var hub = connection.createHubProxy('chatHub');
+﻿'use strict';
+(function($){
+    $(function () {
+        var connection = $.hubConnection();
+        var hub = connection.createHubProxy('chatHub');
 
-    $('#chatForm').on('submit', function (e) {
-        e.preventDefault();
-    });
-
-    hub.on('broadCastMessage', function (time, sender, mssg) {
-        $('#mssgLogger').append('<li><small>[' + time + '] <strong>' + sender + '</strong>:</small> ' + mssg + '</li>');
-    });
-
-
-    connection.start().done(function () {
-        $('#send').click(function (e) {
+        $('#chatForm').on('submit', function (e) {
             e.preventDefault();
+        });
 
-            var sender = $('#textMessage').data('sender');
-            var mssg = $('#textMessage').val();
+        hub.on('broadCastMessage', function (time, sender, mssg) {
+            $('#mssgLogger').append('<li><small>[' + time + '] <strong>' + sender + '</strong>:</small> ' + mssg + '</li>');
+        });
 
-            $('#textMessage').val('');
 
-            hub.invoke('sendMessage', sender, mssg);
-            $("#logger-wrapper").animate({ scrollTop: $("#logger-wrapper").prop("scrollHeight") }, 100);
+        connection.start().done(function () {
+            $('#send').click(function (e) {
+                e.preventDefault();
 
+                var sender = $('#textMessage').data('sender');
+                var mssg = $('#textMessage').val();
+
+                $('#textMessage').val('');
+
+                hub.invoke('sendMessage', sender, mssg);
+                $("#logger-wrapper").animate({ scrollTop: $("#logger-wrapper").prop("scrollHeight") }, 100);
+
+            });
         });
     });
-});
+})(jQuery);
