8d30bd48fa4d93b1e18f53b315b02fb624c2d850
Mina <mina.dodunekova@gmail.com>
2015-04-15 12:42:33 +0300

    Add userSession in the client side to navigate logged in and logged out users

--- Chat.Client/Scripts/ajax-requester.js
+++ Chat.Client/Scripts/ajax-requester.js
@@ -3,8 +3,8 @@
 var ajaxRequester = (function () {
 
     // baseUrl must be changed when deploying application
-    //var baseUrl = "http://localhost:44965/";
-    var baseUrl = "http://lavander-chat.apphb.com/";
+    var baseUrl = "http://localhost:44965/";
+    //var baseUrl = "http://lavander-chat.apphb.com/";
 
     var headers = {};
 
@@ -39,9 +39,15 @@ var ajaxRequester = (function () {
         return makeRequest('POST', null, 'token', data, success, error);
     }
 
+    // Do we need token for that request
+    function logout(success, error) {
+        return makeRequest('POST', null, 'api/Account/Logout', null, success, error);
+    }
+
     return {
         register: register,
-        login: login
+        login: login,
+        logout: logout
     };
 
 })();
---
+++ Chat.Client/Scripts/userSession.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+var userSession = (function () {
+
+    function saveSession(data) {
+        localStorage['currentUser'] = JSON.stringify(data);
+    }
+
+    function getSession() {
+        var userData = localStorage['currentUser'];
+        if (userData) {
+            return JSON.parse(localStorage['currentUser']);
+        }
+    }
+
+    function clearSession() {
+        localStorage.clear();
+    }
+
+    return {
+        save: saveSession,
+        get: getSession,
+        clear: clearSession
+    };
+
+})();
